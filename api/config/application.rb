4505d3b990494f84287525952f5ce35a4b9f4bfa
the3flower <flowerthreeethe@gmail.com>
2025-03-02 17:36:30 +0700

    rubocop

--- api/config/application.rb
+++ api/config/application.rb
@@ -30,11 +30,11 @@ module Api
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:5173'
+        origins "http://localhost:5173"
 
-        resource '*',
+        resource "*",
                  headers: :any,
-                 methods: [:get, :post, :put, :patch, :delete, :options, :head],
+                 methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
                  credentials: true
       end
     end
